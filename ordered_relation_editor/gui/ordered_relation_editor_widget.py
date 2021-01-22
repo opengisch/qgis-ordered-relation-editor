b5bd3438236482f8a73cf66ad802f8e1bb3eca1e
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-01-22 15:38:57 +0100

    displaying QML in widget

--- ordered_relation_editor/gui/ordered_relation_editor_widget.py
+++ ordered_relation_editor/gui/ordered_relation_editor_widget.py
@@ -9,14 +9,26 @@
 # -----------------------------------------------------------
 
 from PyQt5.QtQuickWidgets import QQuickWidget
+import os
+from qgis.PyQt.QtCore import QUrl
+from qgis.PyQt.QtWidgets import QVBoxLayout
+from qgis.PyQt.uic import loadUiType
 from qgis.gui import QgsAbstractRelationEditorWidget
 
+WidgetUi, _ = loadUiType(os.path.join(os.path.dirname(__file__), '../ui/ordered_relation_editor_widget.ui'))
 
-class OrderedRelationEditorWidget(QgsAbstractRelationEditorWidget):
+
+class OrderedRelationEditorWidget(QgsAbstractRelationEditorWidget, WidgetUi):
 
     def __init__(self, config, parent):
         super().__init__(config, parent)
+        self.setupUi(self)
 
+        layout = QVBoxLayout()
+        view = QQuickWidget()
+        view.setSource(QUrl.fromLocalFile(os.path.join(os.path.dirname(__file__), '../qml/OrderedImageList.qml')))
+        layout.addWidget(view)
+        self.mListView.setLayout(layout)
 
     def config(self):
         return {
